5c413d7eb467ff44ba3ccc9be32b56504560621d
jaredabw <jwig0007@student.monash.edu>
2024-06-19 23:46:45 +1000

    readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
                 <h1 className="pb-6 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-dutch_white-700">Jared Wigley</h1>
-                <span className="pt-10 bg-gradient-to-r from-dutch_white-700 from-5% via-ash_gray to-95% to-dark_purple-800 bg-clip-text text-3xl font-bold tracking-tight text-transparent">Full Stack Developer</span>
+                <span className="pt-10 bg-gradient-to-r from-dutch_white-700 from-5% via-ash_gray to-95% to-dark_purple-800 bg-clip-text text-3xl font-bold tracking-tight text-transparent">Software Developer</span>
             </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-0 md:py-6">
         <div className="flex flex-shrink-0 items-center text-dutch_white-700 text-xl md:text-4xl font-extrabold">
-            <a className="hidden px-5 py-2 md:flex" href="/">jaredabw.xyz</a>
-            <a className="flex px-5 py-2 md:hidden" href="/">jaredabw</a>
+            <a className="hidden md:flex py-2 " href="/">jaredabw.xyz</a>
+            <a className="flex md:hidden py-2 " href="/">jaredabw</a>
         </div>
         <div className="m-6 mx-0 sm:mx-10 flex items-center justify-center gap-1 sm:gap-4 text-xl md:text-3xl text-dutch_white-700">
             <a className="p-2" href="https://www.linkedin.com/in/jared-wigley-91921a303/">
