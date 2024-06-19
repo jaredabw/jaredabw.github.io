8c005c48e95dc9e813b10dbae2d10df24d79d31c
jaredabw <jwig0007@student.monash.edu>
2024-06-19 22:49:10 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-    <div className="absolute inset-0 -z-10 size-full items-center px-5 py-24
-    [background:radial-gradient(125%_125%_at_50%_10%,#70A0AF_0%,#706993_100%)]"/>
-    <Navbar/>
+    <div className="overflow-x-hidden antialiased selection:bg-pomp_and_power-400 selection:text-dutch_white-500">
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute inset-0 -z-10 size-full items-center px-5 py-24
+      [background:radial-gradient(125%_125%_at_50%_10%,#70A0AF_10%,#706993_100%)]"/>
+      <div className="container mx-auto px-8">
+        <Navbar/>
+        <Hero/>
+      </div>
+      
+    </div>
     </>
   )
 }
