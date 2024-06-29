7ad7cd5d8c2910c92f4ab004ae231e506060bcb1
jaredabw <jwig0007@student.monash.edu>
2024-06-29 15:39:49 +1000

    add hover action on navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,23 @@ const Navbar = () => {
   return (
     <nav className="mb-8 flex items-center justify-between py-0 md:py-6">
         <div className="flex flex-shrink-0 items-center text-xl md:text-4xl font-extrabold">
-            <a className="hidden md:flex py-2 " href="/">jaredabw.xyz</a>
-            <a className="flex md:hidden py-2 " href="/">jaredabw</a>
+            <a className="hidden md:flex py-2 hover:relative hover:bottom-0.5" href="/">jaredabw.xyz</a>
+            <a className="flex md:hidden py-2 hover:relative hover:bottom-0.5" href="/">jaredabw</a>
         </div>
         <div className="m-6 mx-0 sm:mx-10 flex items-center justify-center gap-1 sm:gap-4 text-xl md:text-3xl text-dutch_white-700">
-            <a className="p-2" href="https://www.linkedin.com/in/jared-wigley-91921a303/">
+            <a className="p-2 hover:relative hover:bottom-0.5" href="https://www.linkedin.com/in/jared-wigley-91921a303/">
                 <FaLinkedin />
             </a>
-            <a className="p-2" href="https://github.com/jaredabw">
+            <a className="p-2 hover:relative hover:bottom-0.5" href="https://github.com/jaredabw">
                 <FaGithub />
             </a>
-            <a className="p-2" href="https://www.instagram.com/jared_abw/">
+            <a className="p-2 hover:relative hover:bottom-0.5" href="https://www.instagram.com/jared_abw/">
                 <FaInstagram />
             </a>
-            <a className="p-2" href="https://discord.com/users/355938178265251842">
+            <a className="p-2 hover:relative hover:bottom-0.5" href="https://discord.com/users/355938178265251842">
                 <FaDiscord />
             </a>
-            <a className="p-2" href="mailto:jabwigley@gmail.com">
+            <a className="p-2 hover:relative hover:bottom-0.5" href="mailto:jabwigley@gmail.com">
                 <FaRegEnvelope />
             </a>
         </div>
