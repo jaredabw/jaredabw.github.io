5208c6021c69b661731c92fa9d2a4cb4275f5d03
jaredabw <jwig0007@student.monash.edu>
2024-07-03 23:13:38 +1000

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Technologies />
           <Experiences />
           <Projects />
+          <Footer />
         </div>
       </div>
     </>
