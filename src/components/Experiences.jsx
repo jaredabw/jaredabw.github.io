be60ef600adc0513478a572bb638e79d12202855
jaredabw <jwig0007@student.monash.edu>
2024-07-03 18:08:15 +1000

    justify right text

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -8,7 +8,7 @@ const Experiences = () => {
                 {EXPERIENCES.map((experience, index) => (
                     <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
                         <div className="w-full lg:w-1/6">
-                            <p className="mb-2 text-m font-bold tracking-wide text-dark_purple-500">{experience.year}</p>
+                            <p className="mb-2 text-m font-bold tracking-wide text-dark_purple-500 text-right mr-20">{experience.year}</p>
                         </div>
                         <div className="w-full max-w-xl lg:w-3/4">
                             <h6 className="mb-2 font-semibold">
