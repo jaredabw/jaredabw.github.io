f33cfbd2e64194236acb4967affb8f87c673776f
jaredabw <jwig0007@student.monash.edu>
2024-07-16 14:21:40 +1000

    update text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="pb-10 pt-10 mt-10 border-t border-opacity-30 border-dutch_white">
       <p className="text-center font-extralight tracking-tight text-opacity-80">
-        Built in Vite with React, Tailwind CSS and Framer Motion. <br />
+        Made in Vite with React, Tailwind CSS and Framer Motion. <br />
         Hosted on Github Pages. <br />
         2024 - Jared Wigley
       </p>
