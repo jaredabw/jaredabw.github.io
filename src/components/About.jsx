7e3c0e05df7b3e79bc8dce7ddcd215766f985b38
jaredabw <jwig0007@student.monash.edu>
2025-03-10 23:28:53 +1100

    remove import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from '../assets/about.jpg'
+// import aboutImg from '../assets/about.jpg'
 import { ABOUT_TEXT } from '../constants'
 import { motion } from 'framer-motion'
 
@@ -11,7 +11,7 @@ const About = () => {
             transition={{ duration: 0.5 }}
             className="my-20 text-center text-4xl">About Me</motion.h2>
             <div className="flex flex-wrap">
-                <motion.div
+                {/* <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 0.5 }}
@@ -19,7 +19,7 @@ const About = () => {
                     <div className="flex items-center justify-center">
                         <img className="rounded-3xl"src={aboutImg} alt="about" />
                     </div>
-                </motion.div>
+                </motion.div> */}
                 <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: 100 }}
