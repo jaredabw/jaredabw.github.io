869cf7b2da4a215d3d65a3e973636ecd74e2446d
jaredabw <jwig0007@student.monash.edu>
2025-04-23 20:27:27 +1000

    adjust pdf viewer

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,21 @@
+import { motion } from "framer-motion";
+
 const Resume = () => {
-    return (
+  return (
+    <div className="pb-4">
+      <motion.h2
+        className="mb-8 text-center text-4xl"
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+      >
+        Resume
+      </motion.h2>
+
       <div className="mx-auto mb-14 w-full xl:w-2/3 h-[600px] md:h-[800px] lg:h-[900px] xl:h-[1000px] 2xl:h-[1200px]">
         <iframe
-          src="https://docs.google.com/document/d/1CRExXupOF9tGGhMHh5IcRmaRqq3KzzHLHjNxjdQesOU/preview"
+          src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://docs.google.com/document/d/1CRExXupOF9tGGhMHh5IcRmaRqq3KzzHLHjNxjdQesOU/export?format=pdf"
           className="w-full h-full border-none"
           title="Resume"
         />
@@ -11,12 +24,13 @@ const Resume = () => {
             href="https://docs.google.com/document/d/1CRExXupOF9tGGhMHh5IcRmaRqq3KzzHLHjNxjdQesOU/export?format=pdf"
             className="hover:underline"
           >
-            If the resume doesn&apos;t load, click here to download it.
+            If the resume isn&apos;t formatted properly, click here to download
+            it.
           </a>
         </p>
       </div>
-    );
-  };
-  
-  export default Resume;
-  
+    </div>
+  );
+};
+
+export default Resume;
