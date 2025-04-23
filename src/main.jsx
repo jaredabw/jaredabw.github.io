2eeda7ee3cac164532c649f72b75c37615f908ed
jaredabw <jwig0007@student.monash.edu>
2025-04-23 20:41:35 +1000

    add ga4 analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import AnalyticsTracker from "./tracker";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/resume" element={<App resumePage />} />
