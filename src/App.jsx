e35b3282f82b8a0bb1b990b5580d4a4130ab455f
jaredabw <jwig0007@student.monash.edu>
2025-04-23 17:08:15 +1000

    change bg colour

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import About from "./components/About";
-import Experiences from "./components/Experiences";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Technologies from "./components/Technologies";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -11,8 +8,10 @@ const App = () => {
     <>
       <div className="overflow-x-hidden antialiased text-dutch_white-700 selection:bg-pomp_and_power-400 selection:text-dutch_white-500">
         <div className="fixed top-0 -z-10 size-full">
-          <div className="absolute inset-0 -z-10 size-full items-center px-5 py-24
-        [background:radial-gradient(125%_125%_at_50%_10%,#70A0AF_10%,#706993_90%)]"/>
+          <div
+            className="absolute inset-0 -z-10 size-full items-center px-5 py-24
+        [background:radial-gradient(125%_125%_at_50%_10%,#000001_50%,#2f244d_95%)]"
+          />
         </div>
 
         <div className="container mx-auto px-8">
@@ -26,7 +25,7 @@ const App = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
