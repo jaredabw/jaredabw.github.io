0f376d35440a6e7e534f01a94d938ca35a09ca84
jaredabw <jwig0007@student.monash.edu>
2026-05-19 00:21:23 +1000

    have download link be animated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -14,14 +14,20 @@ const Resume = () => {
       </motion.h2>
 
       <div className="mx-auto mb-12 w-full xl:w-2/3 h-[600px] md:h-[800px] lg:h-[900px] xl:h-[1000px] 2xl:h-[1200px]">
-        <p className="mb-8 text-center font-thin text-dutch_white-800">
+        <motion.p
+          className="mb-8 text-center font-thin text-dutch_white-800"
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          viewport={{ once: true }}
+        >
           <a
             href="https://docs.google.com/document/d/1CRExXupOF9tGGhMHh5IcRmaRqq3KzzHLHjNxjdQesOU/export?format=pdf"
             className="hover:underline"
           >
             Click here to download.
           </a>
-        </p>
+        </motion.p>
 
         <iframe
           src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://docs.google.com/document/d/1CRExXupOF9tGGhMHh5IcRmaRqq3KzzHLHjNxjdQesOU/export?format=pdf"
