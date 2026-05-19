82085b60e81c80aef10fc0bdf0299869c04d3f03
jaredabw <jwig0007@student.monash.edu>
2026-05-19 11:11:46 +1000

    use up to date year always

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="pb-10 pt-10 mt-10 border-t border-opacity-30 border-dutch_white">
       <p className="text-center font-extralight tracking-tight text-opacity-80 text-sm">
-        2026 — Jared Wigley
+        {new Date().getFullYear()} — Jared Wigley
       </p>
     </div>
   )
